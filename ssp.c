299c5cec2a744e73f0b4f7bb17e071926381afb9
pandiyanmech <35764854+pandiyanmech@users.noreply.github.com>
2018-03-14 15:52:41 +0530

    ssp

---
+++ ssp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+    int n,a[10],i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+      printf("%d %d\n",a[i],i);
+    }
+	return 0;
+  getch();
+}
